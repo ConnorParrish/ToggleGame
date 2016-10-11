6541efd1e8eb31543f12c52049141e7c93c26145
Luke Alexander <Luketheman@gmail.com>
2016-10-11 17:14:45 -0600

    Implemented Main Menu, attaches to Level0 so upon Start Game the level loads. Started to work on the Game Over screen.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string LevelToLoad;
+    public bool IsExit;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("Button Working");
+        if (IsExit)
+        {
+            Application.Quit();
+        }
+        else
+        {
+            SceneManager.LoadScene(LevelToLoad);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ToggleTest/Assets/Script/StartGame.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+
+public class StartGame : MonoBehaviour {
+
+    public string LevelToLoad;
+    public bool isExit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnMouseDown()
+    {
+
+        //If this 'button' is a quit button.
+        if (isExit)
+        {
+            Debug.Log("Quit Working");
+            Application.Quit();
+        }
+        else
+        {
+            SceneManager.LoadScene(LevelToLoad);
+        }
+
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+	    
+	}
+}
