d674845b2a3910ace3cdfcda45af9e84128f0a6e
Luke Alexander <Luketheman@gmail.com>
2016-10-12 11:44:43 -0600

    Very very small change to GameOver Script.

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class GameOverScript : MonoBehaviour {
 
+    public GameObject Player;
+    public bool isDead;
+
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+
+        private GameObject PlayerScript = Player.GetComponent(PlayerMovement);
+}
 	
 	// Update is called once per frame
 	void Update () {
+        if (Player.GetComponent(PlayerMovement) == true)
+        {
+            isDead = true;
+        }
 	
 	}
 }
