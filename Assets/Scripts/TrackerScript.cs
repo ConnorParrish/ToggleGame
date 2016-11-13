03db0ac265fa4d793db15812d27318d621ec541c
Heimdyll <real.heimdyll@gmail.com>
2016-11-13 01:22:46 -0700

    Created a HUDCanvas prefab that includes the level tracker. Adjusted the Player prefab accordingly.

---
+++ Assets/Scripts/TrackerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TrackerScript : MonoBehaviour {
+
+	public Transform startBlock;
+	public Transform endBlock;
+	public Slider progBar;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(startBlock.position);
+		Debug.Log(endBlock.position);
+
+		progBar.maxValue = endBlock.position.x - startBlock.position.x;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		progBar.value = transform.position.x;
+	}
+}
