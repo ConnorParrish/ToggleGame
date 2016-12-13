bb6927eaad82d900845665f0f9cf8b567fb74f3e
Heimdyll <heimdyll@outlook.com>
2016-12-12 23:23:22 -0700

    Added coin tracking to level select menus

--- Assets/LevelSelectProgressTracking.cs
+++ Assets/LevelSelectProgressTracking.cs
@@ -9,21 +9,29 @@ public class LevelSelectProgressTracking : MonoBehaviour {
 	public int coinCount;
 
 	Transform progBar;
+	TextMesh coinTextMesh;
 	SaveScript Saver;
+	int buildIndex;
 	// Use this for initialization
 	void Start () {
 		progBar = transform.GetChild(0);
+		coinTextMesh = transform.GetChild(1).gameObject.GetComponent<TextMesh>();
+		buildIndex = SceneManager.GetActiveScene().buildIndex;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log("Level Select: " + SceneManager.GetActiveScene().buildIndex + " for Level :" + (SceneManager.GetActiveScene().buildIndex - 5));
-		currentProgress = SaveScript.TMD.progress(SceneManager.GetActiveScene().buildIndex - 5);
+		Debug.Log("Level Select: " + buildIndex + " for Level :" + (buildIndex - 5));
+		currentProgress = SaveScript.TMD.progress(buildIndex - 5);
 		if (currentProgress == null){
 			currentProgress = 0;
 		}
 		Debug.Log(currentProgress + " = " + currentProgress*100 + "%");
 		progBar.localScale = new Vector3(currentProgress*100f*2.24f, progBar.localScale.y, progBar.localScale.z);		
-	
+		
+		coinCount = SaveScript.TMD.coinProgress(buildIndex - 5);
+
+		coinTextMesh.text = coinCount + "/5 Coins";
+		
 	}
 }
