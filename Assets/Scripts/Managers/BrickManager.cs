f46cfdf0639f74e5e5481a57eeaf07264f660087
Heimdyll <heimdyll@outlook.com>
2017-03-10 01:07:58 -0700

    Fixed the last of the mobile bugs I had seen.

--- Assets/Scripts/Managers/BrickManager.cs
+++ Assets/Scripts/Managers/BrickManager.cs
@@ -21,35 +21,5 @@ public class BrickManager : MonoBehaviour {
 
 		FirstPlatform = GameObject.FindGameObjectWithTag("WhitePlatform");
 	}
-	// Use this for initialization
-	void Start () {
-		//DONT NEED IT
-		// Debug.Log("Finding objects tagged WhitePlatform...");
 
-	}
-
-	void swapState(GameObject[] platformSet){
-		if (whitePlayer.activeSelf)
-        {
-            whitePlayer.SetActive(false);
-            blackPlayer.SetActive(true);
-        }
-        else
-        {
-            whitePlayer.SetActive(true);
-            blackPlayer.SetActive(false);
-        }
-	}
-	
-	IEnumerator waitpls(){
-		yield return new WaitForSeconds(.2f);
-	}
-
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Q) || ((Input.touchCount == 1) && (Input.GetTouch(0).phase == TouchPhase.Began) && Input.touches[0].position.x > Screen.width/2)){
-			swapState(WhitePlatforms);
-			StartCoroutine(waitpls());
-		}
-	}
 }
